8bf203f0bf9f4a7762583fad023d063dcc1c00ba
Anuraag Oruganti <oanuraag.71@gmail.com>
2026-07-21 12:47:34 +0530

    Add backend/routes/mechanicRoutes.js

---
+++ backend/routes/mechanicRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const protect = require('../middleware/authMiddleware');
+const ctrl = require('../controllers/mechanicController');
+
+router.get('/', ctrl.getAll);
+router.get('/:id', ctrl.getOne);
+router.post('/', protect, ctrl.create);
+router.put('/:id', protect, ctrl.update);
+router.post('/:id/reviews', protect, ctrl.addReview);
+
+module.exports = router;
